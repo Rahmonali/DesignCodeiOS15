65ad6ce41e270a94c59abe15ebb682cf76ebd645
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-19 18:18:12 +0500

    Initial Commit

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by Rahmonali on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Rahmonali on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
