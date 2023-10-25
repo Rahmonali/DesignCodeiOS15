c57f19471ec3ad3c789056dc18df92d52a5533ee
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-25 08:54:07 +0500

    - add course feature with parallax and rotation 3D Effect

---
+++ DesignCodeiOS15/Model/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  DesignCodeiOS15
+//
+//  Created by Rahmonali on 25/10/23.
+//
+
+import SwiftUI
+
+struct Course: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Course()
+}
