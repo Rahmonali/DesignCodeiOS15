e5aaa8ceda13465cdd353fa6a6aa99aca29c0b9e
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-26 10:00:17 +0500

    - hide and show status bar

---
+++ DesignCodeiOS15/Style/Animations.swift
@@ -0,0 +1,18 @@
+//
+//  Animations.swift
+//  DesignCodeiOS15
+//
+//  Created by Rahmonali on 26/10/23.
+//
+
+import SwiftUI
+
+struct Animations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animations()
+}
