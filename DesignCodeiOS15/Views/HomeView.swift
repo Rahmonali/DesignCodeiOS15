61f0c623199735eb2518ec0b17e68d7698d977c1
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-26 09:48:55 +0500

    - transition between views - passing namespace - add Course View - add Course Item

--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -10,18 +10,32 @@ import SwiftUI
 struct HomeView: View {
     
     @State var hasScrolled = false
+    @Namespace var namespace
+    @State var show = false
     
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
             
             ScrollView {
-                
                 scrollDetection
                 
                 featured
                 
-                Color.clear.frame(height: 1000)
+                Text("Courses".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                
+                if !show {
+                    CourseItem(namespace: namespace, show: $show)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                                show.toggle()
+                            }
+                        }
+                }
             }
             .coordinateSpace(name: "scroll")
             .safeAreaInset(edge: .top, content: {
@@ -30,6 +44,10 @@ struct HomeView: View {
             .overlay(
                 NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
             )
+            
+            if show {
+                CourseView(namespace: namespace, show: $show)
+            }
         }
     }
 }
