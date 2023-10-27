9ece71e3d20c2bf10ea17352671d8a5c99028545
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-27 10:23:18 +0500

    - add chaining animation

--- DesignCodeiOS15/Views/CourseView.swift
+++ DesignCodeiOS15/Views/CourseView.swift
@@ -11,16 +11,28 @@ struct CourseView: View {
     
     var namespace: Namespace.ID
     @Binding var show: Bool
+    @State private var appear = [false, false, false]
     
     var body: some View {
         ZStack {
             ScrollView {
                 cover
+                
+                content
+                    .offset(y: 120)
+                    .padding(.bottom, 200)
+                    .opacity(appear[2] ? 1 : 0)
             }
             .background(Color("Background"))
             .ignoresSafeArea()
             
             button
+        }
+        .onAppear {
+            fadeIn()
+        }
+        .onChange(of: show) { newValue in
+            fadeOut()
         }
     }
 }
@@ -68,6 +80,7 @@ extension CourseView {
                     .font(.footnote)
                     .matchedGeometryEffect(id: "text", in: namespace)
                 Divider()
+                    .opacity(appear[0] ? 1 : 0)
                 HStack {
                     Image("Avatar Default")
                         .resizable()
@@ -80,6 +93,7 @@ extension CourseView {
                     Text("Taught by Meng To")
                         .font(.footnote)
                 }
+                .opacity(appear[1] ? 1 : 0)
             }
             .padding(20)
             .background {
@@ -91,6 +105,20 @@ extension CourseView {
             .offset(y: 250)
             .padding(20)
         }
+    }
+    
+    private var content: some View {
+        VStack(alignment: .leading, spacing: 30) {
+            Text("SwiftUI helps you build great-looking apps across all Apple platforms with the power of Swift — and surprisingly little code.")
+                .font(.title3).fontWeight(.medium)
+            Text("This course")
+                .font(.title).bold()
+            Text("Build sophisticated animations with expanded animation support. Use phases to create sequences of animations, or create multiple animation tracks using keyframes. SwiftUI automatically transfers the velocity of a user gesture to your animations so your app feels fluid and natural.")
+            Text("Share more of your SwiftUI code with your watchOS apps. Scroll vertical TabViews using the crown, match colors with adaptive background containers, take advantage of edge-to-edge displays with new ToolbarItem placements, and leverage NavigationSplitView to build detailed list views.")
+                .font(.title.bold())
+            Text("When you recompile your SwiftUI apps for visionOS, you can add depth and 3D objects to windows or present volumes. Use RealityView to bring in RealityKit content alongside your views and controls. And you can use SwiftUI and RealityKit together to build Full Space immersive experiences.")
+        }
+        .padding(20)
     }
     
     private var button: some View {
@@ -110,5 +138,25 @@ extension CourseView {
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
         .padding(20)
         .ignoresSafeArea()
+    }
+}
+
+extension CourseView {
+    func fadeIn() {
+        withAnimation(.easeOut.delay(0.3)) {
+            appear[0] = true
+        }
+        withAnimation(.easeOut.delay(0.3)) {
+            appear[1] = true
+        }
+        withAnimation(.easeOut.delay(0.3)) {
+            appear[2] = true
+        }
+    }
+    
+    func fadeOut() {
+        appear[0] = false
+        appear[1] = false
+        appear[2] = false
     }
 }
