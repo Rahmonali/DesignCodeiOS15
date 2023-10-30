74c4f55efa0d0a658276eb4ffa805a6f8c41fc87
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-30 18:16:39 +0500

    - add Rich text with markdown - add custom input style - add animated focus states

--- DesignCodeiOS15/Views/SignUpView.swift
+++ DesignCodeiOS15/Views/SignUpView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct SignUpView: View {
+    enum Field: Hashable {
+        case email
+        case password
+    }
+    
+    @State private var email = ""
+    @State private var password = ""
+    @FocusState var focusedField: Field?
+    @State var circleY: CGFloat = 120
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("Sign up")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
+            
+            TextField("Email", text: $email)
+                .inputStyle(icon: "mail")
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+                .focused($focusedField, equals: .email)
+                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+            
+            SecureField("Password", text: $password)
+                .inputStyle(icon: "lock")
+                .textContentType(.password)
+                .focused($focusedField, equals: .password)
+                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
             
             Button(action: {}, label: {
                 Text("Create an account")
@@ -24,9 +49,33 @@ struct SignUpView: View {
             .buttonStyle(.angular)
             .tint(.accentColor)
             .controlSize(.large)
+            
+            Group {
+                Text("By clicking on ")
+                + Text("_Create an account_").foregroundColor(Color.primary.opacity(0.7))
+                + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://designcode.io)**")
+                
+                Divider()
+                
+                HStack {
+                    Text("Already have an account?")
+                    Button(action: {}, label: {
+                        Text("**Sign In**")
+                    })
+                }
+            }
+            .font(.footnote)
+            .foregroundColor(Color.secondary)
+            .accentColor(Color.secondary)
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background (
+            Circle().fill(.blue)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+        )
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
         .padding(20)
@@ -34,9 +83,23 @@ struct SignUpView: View {
             Image("Blob 1")
                 .offset(x: 200, y: -100)
         )
+        .onChange(of: focusedField) { value in
+            withAnimation {
+                if value == .email {
+                    circleY = 120
+                } else {
+                    circleY = 190
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    SignUpView()
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            SignUpView()
+                .preferredColorScheme(.light)
+        }
+    }
 }
