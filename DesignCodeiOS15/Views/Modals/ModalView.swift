05e6a386ed39ba6533ff1aad68a8a176afbfcfa2
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-10-31 16:20:11 +0500

    - dismiss modal animation

--- DesignCodeiOS15/Views/Modals/ModalView.swift
+++ DesignCodeiOS15/Views/Modals/ModalView.swift
@@ -11,10 +11,15 @@ struct ModalView: View {
     @EnvironmentObject var model: Model
     @AppStorage("showModal") var showModal = true
     @State private var viewState: CGSize = .zero
+    @State private var isDismiss = false
+    @State var appear = [false, false, false]
     
     var body: some View {
         ZStack {
             Color.clear.background(.regularMaterial)
+                .onTapGesture {
+                    dismissModal()
+                }
                 .ignoresSafeArea()
             
             Group {
@@ -25,6 +30,7 @@ struct ModalView: View {
             }
             .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .offset(x: viewState.width, y: viewState.height)
+            .offset(y: isDismiss ? 1000 : 0)
             .rotationEffect(.degrees(viewState.width / 40))
             .rotation3DEffect(
                 .degrees(viewState.height / 20), axis: (x: 1.0, y: 0.0, z: 0.0)
@@ -32,16 +38,20 @@ struct ModalView: View {
             .hueRotation(.degrees(viewState.width / 5))
             .gesture(drag)
             .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+            .opacity(appear[0] ? 1 : 0)
+            .offset(y: appear[0] ? 0 : 200)
             .padding(20)
             .background(
                 Image("Blob 1")
                     .offset(x: 200, y: -100)
+                    .opacity(appear[2] ? 1 : 0)
+                    .offset(y: appear[2] ? 0 : 10)
+                    .blur(radius: appear[2] ? 0 : 40)
+                    .allowsHitTesting(false)
             )
             
             Button(action: {
-                withAnimation {
-                    showModal = false
-                }
+                dismissModal()
             }, label: {
                 Image(systemName: "xmark")
                     .font(.body.weight(.bold))
@@ -53,6 +63,19 @@ struct ModalView: View {
             })
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
             .padding(20)
+            .opacity(appear[1] ? 1 : 0)
+            .offset(y: appear[1] ? 0 : -200)
+        }
+        .onAppear {
+            withAnimation(.easeOut) {
+                appear[0] = true
+            }
+            withAnimation(.easeOut.delay(0.1)) {
+                appear[1] = true
+            }
+            withAnimation(.easeOut(duration: 1).delay(0.2)) {
+                appear[2] = true
+            }
         }
     }
 }
@@ -65,9 +88,24 @@ extension ModalView {
             }
             .onEnded { value in
                 withAnimation(.openCard) {
-                    viewState = .zero
+                    if value.translation.height > 200 {
+                        dismissModal()
+                    } else {
+                        withAnimation(.openCard) {
+                            viewState = .zero
+                        }
+                    }
                 }
             }
+    }
+    
+    func dismissModal() {
+        withAnimation {
+            isDismiss = true
+        }
+        withAnimation(.linear.delay(0.3)) {
+            showModal = false
+        }
     }
 }
 
--- DesignCodeiOS15/Views/Modals/SignUpView.swift
+++ DesignCodeiOS15/Views/Modals/SignUpView.swift
@@ -20,51 +20,56 @@ struct SignUpView: View {
     @State private var emailY: CGFloat = 0
     @State private var passwordY: CGFloat = 0
     @State private var circleColor: Color = .blue
+    @State private var appear = [false, false, false]
     @EnvironmentObject var model: Model
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("Sign up")
                 .font(.largeTitle).bold()
+                .opacity(appear[0] ? 1 : 0)
+                .offset(y: appear[0] ? 0 : 20)
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
-            
-            TextField("Email", text: $email)
-                .inputStyle(icon: "mail")
-                .textContentType(.emailAddress)
-                .keyboardType(.emailAddress)
-                .textInputAutocapitalization(.never)
-                .autocorrectionDisabled()
-                .focused($focusedField, equals: .email)
-                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
-                .overlay(geometry)
-                .onPreferenceChange(CirclePreferenceKey.self) { value in
-                    emailY = value
-                    circleY = value
-                }
-            
-            SecureField("Password", text: $password)
-                .inputStyle(icon: "lock")
-                .textContentType(.password)
-                .focused($focusedField, equals: .password)
-                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
-                .overlay(geometry)
-                .onPreferenceChange(CirclePreferenceKey.self) { value in
-                    passwordY = value
-                }
-            
-            Button(action: {}, label: {
-                Text("Create an account")
-                    .frame(maxWidth: .infinity)
-            })
-            .font(.headline)
-            .blendMode(.overlay)
-            .buttonStyle(.angular)
-            .tint(.accentColor)
-            .controlSize(.large)
-            .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+                .opacity(appear[1] ? 1 : 0)
+                .offset(y: appear[1] ? 0 : 20)
             
             Group {
+                TextField("Email", text: $email)
+                    .inputStyle(icon: "mail")
+                    .textContentType(.emailAddress)
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    .focused($focusedField, equals: .email)
+                    .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                    .overlay(geometry)
+                    .onPreferenceChange(CirclePreferenceKey.self) { value in
+                        emailY = value
+                        circleY = value
+                    }
+                
+                SecureField("Password", text: $password)
+                    .inputStyle(icon: "lock")
+                    .textContentType(.password)
+                    .focused($focusedField, equals: .password)
+                    .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                    .overlay(geometry)
+                    .onPreferenceChange(CirclePreferenceKey.self) { value in
+                        passwordY = value
+                    }
+                
+                Button(action: {}, label: {
+                    Text("Create an account")
+                        .frame(maxWidth: .infinity)
+                })
+                .font(.headline)
+                .blendMode(.overlay)
+                .buttonStyle(.angular)
+                .tint(.accentColor)
+                .controlSize(.large)
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
+                
                 Text("By clicking on ")
                 + Text("_Create an account_").foregroundColor(Color.primary.opacity(0.7))
                 + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://designcode.io)**")
@@ -79,10 +84,12 @@ struct SignUpView: View {
                         Text("**Sign In**")
                     })
                 }
+                .font(.footnote)
+                .foregroundColor(Color.secondary)
+                .accentColor(Color.secondary)
             }
-            .font(.footnote)
-            .foregroundColor(Color.secondary)
-            .accentColor(Color.secondary)
+            .opacity(appear[2] ? 1 : 0)
+            .offset(y: appear[2] ? 0 : 20)
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
@@ -103,6 +110,17 @@ struct SignUpView: View {
                     circleY = passwordY
                     circleColor = .red
                 }
+            }
+        }
+        .onAppear {
+            withAnimation(.spring.delay(0.1)) {
+                appear[0] = true
+            }
+            withAnimation(.spring.delay(0.2)) {
+                appear[1] = true
+            }
+            withAnimation(.spring.delay(0.3)) {
+                appear[2] = true
             }
         }
     }
