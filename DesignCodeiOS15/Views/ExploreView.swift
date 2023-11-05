5b7bc8e6bfebd524572005528713637068c45857
Rahmonali Fatkhuddinov <rahmonali1995@gmail.com>
2023-11-05 15:44:44 +0500

    - add play Button - add Explore view

--- DesignCodeiOS15/Views/ExploreView.swift
+++ DesignCodeiOS15/Views/ExploreView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.background.ignoresSafeArea()
+            
+            coursesSection
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(NavigationBar(title: "Recent", hasScrolled: .constant(true)))
+            .background(Image("Blob 1").offset(x: -100, y: -400))
+        }
+    }
+}
+
+extension ExploreView  {
+    private var coursesSection: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack (spacing: 16){
+                ForEach(courses) { course in
+                    SmallCourseItem(course: course)
+                }
+            }
+            .padding(.horizontal, 20)
+            Spacer()
+        }
     }
 }
 
